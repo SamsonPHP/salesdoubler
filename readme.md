6c2b08ec57075e8717adec65c4b42956c6a9c0b4
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-12-17 16:06:38 +0200

    Init

---
+++ readme.md
@@ -0,0 +1,9 @@
+# SamsonPHP [SalesDoubler](salesdoubler.com.ua) CPA i
+
+[![Latest Stable Version](https://poser.pugx.org/samsonphp/salesdoubler/v/stable.svg)](https://packagist.org/packages/samsonphp/salesdoubler)
+[![Build Status](https://scrutinizer-ci.com/g/samsonphp/salesdoubler/badges/build.png?b=master)](https://scrutinizer-ci.com/g/samsonphp/salesdoubler/build-status/master)
+[![Code Coverage](https://scrutinizer-ci.com/g/samsonphp/salesdoubler/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/samsonphp/salesdoubler/?branch=master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/samsonphp/salesdoubler/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/samsonphp/salesdoubler/?branch=master) 
+[![Total Downloads](https://poser.pugx.org/samsonphp/salesdoubler/downloads.svg)](https://packagist.org/packages/samsonphp/salesdoubler)
+[![Stories in Ready](https://badge.waffle.io/samsonphp/salesdoubler.png?label=ready&title=Ready)](https://waffle.io/samsonphp/salesdoubler)
+
---
+++ src/Module.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VITALYIEGOROV
+ * Date: 17.12.15
+ * Time: 15:40
+ */
+namespace samsonphp\salesdoubler;
+
+use samson\core\CompressableService;
+
+/**
+ * SalesDoubler.com.ua CPA module inplementation.
+ *
+ * @package samsonphp\salesdoubler
+ */
+class Module extends CompressableService
+{
+    /** @var string User cookie identifier */
+    public $cookieID = 'salesdoubler_clickid';
+
+    /** @var string Cookie life period */
+    public $cookiePeriod = 2592000;
+
+    /** @var string User click GET param name */
+    public $getParamName = 'aff_sub';
+
+    /** @var string RegExp pattern for matching paid channels */
+    public $paidPattern = '/utm_medium=/';
+
+    /** @var string SalesDoubler post back url */
+    public $postbackUrl = 'http://rdr.salesdoubler.com.ua/in/postback/@id/@clickid?trans_id=@transid&token=@token';
+
+    /** @var string SalesDoubler post back url token value */
+    public $postbackToken = 'bG92ZWdpZnRkdXJleEBzYWxlc2RvdWJsZXIuY29tLnVh';
+
+    /** @var string SalesDoubler post back url identifier value */
+    public $postbackID = '1240';
+
+    /** @var string Current user internal identifier */
+    public $transID;
+
+    /** @var string Identifier */
+    protected $id = 'SalesDoubler';
+
+    /**
+     * Initialize module.
+     *
+     * @param array $params Paramters
+     * @return bool Module initialization results
+     */
+    public function init(array $params = array())
+    {
+        // If this is paid channel
+        if (isset($_COOKIE[$this->cookieID]) && preg_match($this->paidPattern, url()->text)) {
+            // Remove cookie if use has come from paid channel
+            unset($_COOKIE[$this->cookieID]);
+            setcookie($this->cookieID, null, -1, '/');
+            // Otherwise search for saledoubler get parameter
+        } elseif (isset($_GET[$this->getParamName]) && !empty($_GET[$this->getParamName])) {
+            $_SESSION[$this->cookieID] = $_GET[$this->getParamName];
+            // Generate random internal trans identifier
+            $this->transID = md5(rand(1, 1000) . $_GET[$this->getParamName] . time() . rand(1, 1000));
+            setcookie($this->cookieID, $_GET[$this->getParamName], time() + $this->cookiePeriod);
+        }
+    }
+
+    /**
+     * Trigger success goal action
+     */
+    public function goal()
+    {
+        // If we have salesdoubler cookie and CURL is working
+        if ($_COOKIE[$this->cookieID] && $curl = curl_init()) {
+            $url = str_replace('@clickid', $_COOKIE[$this->cookieID], $this->postbackUrl);
+            $url = str_replace('@token', $this->postbackToken, $url);
+            $url = str_replace('@id', $this->postbackID, $url);
+            $url = str_replace('@transid', $this->transID, $url);
+
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_exec($curl);
+            curl_close($curl);
+        }
+    }
+}
